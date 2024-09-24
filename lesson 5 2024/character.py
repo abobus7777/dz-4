a6576c27d2ad0b2d0abc8932dc095ecac5f3984b
Kampot <lynnik.vova@gmail.com>
2024-09-24 17:59:35 +0300

    init

--- lesson 5 2024/character.py
+++ lesson 5 2024/character.py
@@ -4,4 +4,26 @@ class Character:
     damage = 1
     defense = 0
 
-    def __init__ (self):
+    def __init__ (self,name,health,damage,defense, type='human'):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.defense = defense
+        self.type = type
+
+    def show_stats(self):
+        print(f"-- {self.name} --\nhealth {self.health}\n "
+        f"damage : {self.damage}\ndefense: {self.defense}")
+
+    def ___str___(self):
+        return(f"-- {self.name} --\nhealth {self.health}\n "
+        f"damage : {self.damage}\ndefense: {self.defense}")
+
+    def take_damage(self, damage):
+       # self.health -= damage
+       # if self.health < 0:
+       #     self.health = 0
+       self.health = max(self.health - damage, 0)
+
+    def attack(self, target):
+        target.take_damage(self.damage)
---
+++ lesson 5 2024/lesson five.py
@@ -0,0 +1,27 @@
+from character import Character
+from lesson f  import Paladin
+player1 = Character('grawlactor', 500, 20,10)
+player1.show_stats()
+
+player2 = Paladin('Nequard', 200, 20,7)
+player2.show_stats()
+
+player1.attack(player2)
+
+print(f"\n{player1}\n{player2}\n")
+
+a=3
+b=5
+
+print(id(a))
+print(id(b))
+
+class Paladin(Character):
+    def __init__(self, name, health, damage,defense ,type = 'human'):
+        Character.__init__(self, name ,health, damage,defense ,type )
+    def take_damage (self,damage):
+        damage-=
+
+    def take_damage(self, damage):
+        damage -=self.defense
+        Character.take_damage(self, damage)
